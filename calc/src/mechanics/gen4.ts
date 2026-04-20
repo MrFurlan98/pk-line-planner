0fd9486424f7ed21f0ab3a15c1ee90ee68070b3c
anastarawneh <anastarawneh2001@icloud.com>
2026-04-20 22:10:07 +0300

    Change Rock Wrecker and Triple Kick again...

--- calc/src/mechanics/gen4.ts
+++ calc/src/mechanics/gen4.ts
@@ -431,11 +431,11 @@ export function calculateBasePowerDPP(
     basePower = hit * 20;
     desc.moveBP = move.hits === 2 ? 60 : move.hits === 3 ? 120 : 20;
     break;
-    // 30 -> 40 -> 50
+    // 30 -> 60 -> 90
   case 'Triple Kick':
   case 'Rock Wrecker':
-    basePower = 20 + hit * 10;
-    desc.moveBP = move.hits === 2 ? 70 : move.hits === 3 ? 120 : 30;
+    basePower = hit * 30;
+    desc.moveBP = move.hits === 2 ? 90 : move.hits === 3 ? 180 : 30;
     break;
   default:
     basePower = move.bp;
